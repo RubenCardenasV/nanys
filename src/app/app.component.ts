1a438704a1f5657e035f4ecf3afaa89aafeb5974
Ruben2412 <ruben.cardenas@innovacionescomputacionalesvl.com>
2024-05-09 17:06:48 -0700

    [navigation]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'kanwu';
-  img: string ='./assets/BGMonterrey.jpg';
-  rutaLogo: string = "./assets/logo.jpg";
-  inicio: string = "Inicio"
+  img: string = './assets/BGMonterrey.jpg';
+  rutaLogo: string = './assets/logo.jpg';
+  inicio: string = 'Inicio';
 
+  urlInstagram: string = 'https://www.instagram.com/kanwucol/';
 
   openWhatsapp() {
     window.open('https://api.whatsapp.com/send?phone=526648223954', '_blank');
+  }
+
+  scrollTo(sectionId: string) {
+    var section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 }
