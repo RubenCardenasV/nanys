37e1234574d16e0cd313e49591b1a787db2662c4
Ruben2412 <ruben.cardenas@innovacionescomputacionalesvl.com>
2024-05-17 12:21:05 -0700

    [corredion posiciones]

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener  } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-navbar',
@@ -7,12 +8,35 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
   rutaLogo: string = './assets/logo.jpg';
+  isNavbarVisible: boolean = true;
+  lastScrollPosition: number = 0;
 
   scrollTo(sectionId: string) {
     var section = document.getElementById(sectionId);
     if (section) {
       section.scrollIntoView({ behavior: 'smooth' });
     }
+  }
+
+  ngOnInit(): void {
+    $('.js-scroll-trigger').click(() => {
+      $('.navbar-collapse').collapse('hide');
+    });
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScrollPosition = window.pageYOffset;
+
+    if (currentScrollPosition > this.lastScrollPosition) {
+      // Desplazamiento hacia abajo, ocultar la barra de navegación
+      this.isNavbarVisible = false;
+    } else {
+      // Desplazamiento hacia arriba, mostrar la barra de navegación
+      this.isNavbarVisible = true;
+    }
+
+    this.lastScrollPosition = currentScrollPosition;
   }
   
 }
